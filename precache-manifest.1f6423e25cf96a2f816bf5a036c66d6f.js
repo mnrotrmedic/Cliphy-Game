9ca201ea4bdb6ebd19b0ef7ff8be8104b9a45c14
mnrotrmedic <33576579+mnrotrmedic@users.noreply.github.com>
2019-04-29 21:46:23 -0500

    Updates

---
+++ precache-manifest.1f6423e25cf96a2f816bf5a036c66d6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "205bf7f9770f2172e427575996bb5352",
+    "url": "/cliphy-game/index.html"
+  },
+  {
+    "revision": "7365abc6a76bb8c82434",
+    "url": "/cliphy-game/static/css/2.c489bcc4.chunk.css"
+  },
+  {
+    "revision": "56fe88a27d0c2500296b",
+    "url": "/cliphy-game/static/css/main.da80fe2a.chunk.css"
+  },
+  {
+    "revision": "7365abc6a76bb8c82434",
+    "url": "/cliphy-game/static/js/2.bba677ee.chunk.js"
+  },
+  {
+    "revision": "56fe88a27d0c2500296b",
+    "url": "/cliphy-game/static/js/main.ec411c06.chunk.js"
+  },
+  {
+    "revision": "f119a71fdb5563c673ce",
+    "url": "/cliphy-game/static/js/runtime~main.75aabe6f.js"
+  }
+]);
